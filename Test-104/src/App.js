05b7d83afddd1a2ccbc161ef93ad13e3802af459
Isaac Ishola <isaacdarepyto@gmail.com>
2022-05-25 09:23:29 +0100

    A SIMPLE CALCULATOR

---
+++ Test-104/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Practiceasync from './Components/Practiceasync';
+
+function App() {
+  return (
+    <div >
+      <Practiceasync />
+     
+    </div>
+  );
+}
+
+export default App;
